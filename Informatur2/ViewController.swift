91be86a0468c5661dcd4e35d8c87808053b1c243
arsanhueza <arsanhueza@udec.cl>
2015-01-22 20:11:30 -0300

    Initial Commit

---
+++ Informatur2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Informatur2
+//
+//  Created by Arturo Sanhueza on 22-01-15.
+//  Copyright (c) 2015 Arturo Sanhueza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Informatur2Tests/Informatur2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Informatur2Tests.swift
+//  Informatur2Tests
+//
+//  Created by Arturo Sanhueza on 22-01-15.
+//  Copyright (c) 2015 Arturo Sanhueza. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Informatur2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
